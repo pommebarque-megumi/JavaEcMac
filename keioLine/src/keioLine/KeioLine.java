aff11645f6a6c108b814b13d8c9ae4b3150002fc
pommebarque-megumi <cache2gecko@gmail.com>
2022-05-10 12:39:17 +0900

    JPEG追加

--- keioLine/src/keioLine/KeioLine.java
+++ keioLine/src/keioLine/KeioLine.java
@@ -19,17 +19,17 @@ public class KeioLine {
 	}
 	public void trip(ArrayList<String> station) throws InterruptedException {
 		for(String s : station) {
-				System.out.println(s);
-				if(s.equals("東府中")) {
-					System.out.println("[府中競馬場前行きにお乗り換えできます]");
-				}else if(s.equals("高幡不動")) {
-					System.out.println("[多摩動物公園行きにお乗り換えできます]");
-				}else {;}
-				Thread.sleep(750);
+			System.out.println(s);
+			if(s.equals("東府中")) {
+				System.out.println("[府中競馬場前行きにお乗り換えできます]");
+			}else if(s.equals("高幡不動")) {
+				System.out.println("[多摩動物公園行きにお乗り換えできます]");
+			}else {;}
+			Thread.sleep(750);
 		}
 		System.out.printf("『終点%sでございます。』%n『ご乗車ありがとうございました。足元にご注意ください。』%n",station.get(station.size()-1));
 	}
-//乗る用のメソッド
+	//乗る用のメソッド
 	public void getOnDown() {
 		System.out.printf("【%sに乗車します】%n『この列車は、各駅停車%s行きです』%n",this.name,this.stationDown.get(stationDown.size()-1));
 	}
